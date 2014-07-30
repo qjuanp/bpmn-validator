ac2969064fc4ce90deef7b3044640cff910be012
Juan P. <i@qjuanp.net>
2014-07-30 01:37:39 -0500

    Implemented validation Style 0123

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ View live demo at: http://bpmn-validator.azurewebsites.net/
 - [X] Style 0104.  Two activities in the same process should not have the same name.  (Use global activity to reuse a single activity in a process.) [1][1]
 - [X] Style 0115.  A throwing intermediate event should be labeled. [1][1]
 - [X] Style 0122.  A catching Message event should have incoming message flow.[1][1]
-- [ ] Style 0123.  A throwing Message event should have outgoing message flow.[1][1]
+- [X] Style 0123.  A throwing Message event should have outgoing message flow.[1][1]
 - [ ] BPMN 0102.  All flow objects other than end events and compensating activities must have an outgoing sequence flow, if the process level includes any start or end events.[1][1]
 
 ##Static Design
--- src/Q.BPMN.Validator/Q.BPMN.Validator/Handlers/Style0123.cs
+++ src/Q.BPMN.Validator/Q.BPMN.Validator/Handlers/Style0123.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Q.BPMN.Validator.Enums;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Q.BPMN.Validator.Extensions;
 
 namespace Q.BPMN.Validator.Handlers
 {
@@ -23,7 +27,51 @@ namespace Q.BPMN.Validator.Handlers
 
         public override void Validate(Contexts.ValidationContext context)
         {
-            throw new NotImplementedException();
+            Debug.WriteLine("Validate::{0}-{1}", Type.ToString(), Id);
+            XDocument currentDocument = context.Document;
+
+            var queryThrowMessages = from el in currentDocument
+                                                    .Descendants(XName.Get("TriggerResultMessage", XPDLDefinition.SCHEMA))
+                                     where el.HasAttributes && el.Attribute("CatchThrow").Value == "THROW"
+                                     select el;
+
+            var queryActivities = (from el in queryThrowMessages
+                                   from ancestor in el.Ancestors(XName.Get("Activity", XPDLDefinition.SCHEMA))
+                                   select ancestor).ToList();
+
+            var queryMessageFlow = (from ac in queryActivities
+                                    join fl in currentDocument
+                                                           .Descendants(XName.Get("MessageFlow", XPDLDefinition.SCHEMA))
+                                            on ac.Attribute("Id").Value equals fl.Attribute("Source").Value into ps
+                                    from p in ps.DefaultIfEmpty()
+                                    select new { ActivityId = p == null ? ac.Attribute("Id").Value : string.Empty })
+                                   .Select(acid => acid.ActivityId);
+
+            var queryActivityWithError = from ac in currentDocument
+                                                        .Descendants(XName.Get("Activity", XPDLDefinition.SCHEMA))
+                                         from id in queryMessageFlow
+                                         where ac.Attribute("Id").Value == id
+                                         select ac;
+
+            if (queryActivityWithError.Count() > 0) ReportError(context, queryActivityWithError.ToList());
+
+            if (Successor != null) Successor.Validate(context);
+        }
+
+        private void ReportError(Contexts.ValidationContext context, List<XElement> activities)
+        {
+            foreach (XElement activity in activities)
+            {
+                context.Errors.Add(new Models.ValidationError
+                {
+                    ElementId = new Guid(activity.Attribute("Id").Value),
+                    ElementName = activity.Attribute("Name").Value,
+                    ElementXPath = activity.GetAbsoluteXPath(),
+                    Id = Id,
+                    Type = Type,
+                    Message = string.Format("Message '{0}' should have an incoming message", activity.Attribute("Name").Value)
+                });
+            }
         }
     }
 }
